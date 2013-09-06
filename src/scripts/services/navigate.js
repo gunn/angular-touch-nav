f493af680a8d3b760101d5a8e1546952eecb7b4b
Arthur Gunn <arthur@gunn.co.nz>
2013-09-06 22:37:52 +1200

    Add methods for working with browser history.

--- src/scripts/services/navigate.js
+++ src/scripts/services/navigate.js
@@ -5,11 +5,15 @@ var forEach = angular.forEach;
 angular.module('ngTouchNav')
 
   //expose $navigate service inside views
-  .run(function($rootScope, $navigate) {
+  .run(function($rootScope, $navigate, $location) {
     $rootScope.$navigate = $navigate;
     $rootScope.$redraw = function performRedraw() {
       document.body.removeChild(document.body.appendChild(document.createElement('style')));
     };
+
+    $rootScope.$on('$locationChangeSuccess', function () {
+      $navigate.handleBrowserNavigation($location.path());
+    });
   })
 
   .provider('$navigate', function() {
@@ -18,12 +22,39 @@ angular.module('ngTouchNav')
 
       var self = this;
       this.history = [];
+      this.historyIndex = 0;
+
       this.defaultAnimation = 'animation-slide';
       this.animation = this.defaultAnimation;
 
       setTimeout(function() {
         self.history.push({path: $location.path(), animation: null});
       });
+
+      this.indexForPath = function (path) {
+        for (var i = 0; i < this.history.length; i++) {
+          if (this.history[i].path == path) {
+            return i;
+          }
+        }
+      };
+
+      this.handleBrowserNavigation = function (path) {
+        var currentItem = this.history[this.historyIndex];
+        if (currentItem && currentItem.path != path) {
+          // We didn't get here via $navigate.path(path)
+          var index = this.indexForPath(path);
+
+          if (index!=undefined && index!=this.historyIndex) {
+            // We've been here before
+            var reverse = index < this.historyIndex;
+            this.setAnimation(currentItem.animation || this.defaultAnimation, reverse);
+
+            if (reverse) this.historyIndex--;
+            else         this.historyIndex++;
+          }
+        }
+      };
 
       this.setAnimation = function(animation, reverse) {
         if(animation) {
@@ -44,8 +75,21 @@ angular.module('ngTouchNav')
         }
         this.setAnimation(animation || this.defaultAnimation, reverse);
         $location.path(path);
-        if(!reverse) {
-          this.history.push({path: path, animation: animation});
+
+        if (reverse) {
+          this.historyIndex--;
+        } else {
+          // Mimic browser behaviour of removing forward-history on normal navigation
+          while (this.history.length > this.historyIndex+1) {
+            this.history.pop();
+          }
+
+          this.historyIndex++;
+
+          this.history.push({
+            path: path,
+            animation: animation
+          });
         }
       };
 
@@ -59,8 +103,8 @@ angular.module('ngTouchNav')
         if(this.history.length < 2) {
           return;
         }
-        var animation = this.history.pop().animation;
-        var item = this.history[this.history.length - 1];
+        var animation = this.history[this.historyIndex].animation;
+        var item = this.history[this.historyIndex - 1];
         if(item.path) {
           this.path(item.path, animation, true);
         }
